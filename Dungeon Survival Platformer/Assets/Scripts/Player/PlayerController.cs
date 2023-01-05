e3fac720421cc79e4bff1d37b59d2b40b41284ca
TheToxicChemist01 <64322975+TheToxicChemist01@users.noreply.github.com>
2023-01-05 20:32:40 +0200

    Created Animations and movement
    Created animations of player attacking, added jump mechanics and some basic background imports to start level creation. Added jump and created main tilemap

--- Dungeon Survival Platformer/Assets/Scripts/Player/PlayerController.cs
+++ Dungeon Survival Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -8,18 +8,26 @@ public class PlayerController : MonoBehaviour
     float movementSpeed = 2f;
     [SerializeField]
     float jumpHeight = 5f;
+    
+    float dirX;
+
+
+    [SerializeField]
+    LayerMask jumpableGround;
 
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
     Animator anim;
+    BoxCollider2D coll;
 
-    [SerializeField]
-    float dirX;
+    enum MovementState { idle, running, jumping, falling, attacking}
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -29,28 +37,51 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(dirX * movementSpeed, rb.velocity.y);
 
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(0, jumpHeight);
+            rb.velocity = new Vector2(0f, jumpHeight);
         }
-        AnimationUpdate();
+        UpdateAnimationState();
     }
 
-    void AnimationUpdate()
+    void UpdateAnimationState()
     {
-        if (dirX > 0)
+        MovementState state;
+
+        if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             spriteRenderer.flipX = false;
         }
-        else if (dirX < 0)
+        else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             spriteRenderer.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            state = MovementState.attacking;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f,jumpableGround);
     }
 }
